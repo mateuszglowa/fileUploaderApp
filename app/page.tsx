6c5573252f64c10772d0adffe165da6e01e83f99
Mateusz Glowa <glowa.mateusz@gmail.com>
2025-03-20 16:09:55 +0100

    add button to the app.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div><p className="text-3xl font-medium text-sky-400">Hello World!</p></div>
+    <div><p className="text-3xl font-medium text-sky-400">Hello World!</p>
+    <Button variant="outline">Button</Button>
+    </div>
   );
 }
